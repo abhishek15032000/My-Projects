1bf6bf2aa8ee9c70e736d1c2feab8c78abe711c5
Abhishek Singh <56187643+abhishek15032000@users.noreply.github.com>
2023-02-21 18:00:03 -0800

    Add files via upload

---
+++ ReduxCart/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Link,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+import { Provider } from "react-redux";
+import store from "./store/store";
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+          </Routes>
+        </Router>
+      </Provider>
+    </div>
+  );
+}
+
+export default App;
